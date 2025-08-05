f301b39e8cdc5b15ce5ca0aa2ad31459b9e270c1
Annick Kere <annickkere@gmail.com>
2025-08-05 23:32:36 +0000

    Refractoring App.jsx, Main.jsx and db.json, deleting index.css and unfilling App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+// Importation des Composants
+import UpdateClient from './components/UpdateClient'
+import ClientDetail from './components/ClientDetail'
+import CreateClient from './components/CreateClient'
+import ClientList from './components/ClientList'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Application React CRUD </h2>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="*" element={<Navigate to="/clients" replace />} />
+        <Route path="/clients" element={<ClientList />} />
+        <Route path="/clients/create" element={<CreateClient />} />
+        <Route path="/clients/:id" element={<ClientDetail />} />
+        <Route path="/clients/:id/update" element={<UpdateClient />} />
+      </Routes>
+    </Router>
   )
 }
 
